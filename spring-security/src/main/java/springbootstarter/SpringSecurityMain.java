1089f070c8712696b82ea02a60b9ff87656e380f
prerna0995 <82801063+prerna0995@users.noreply.github.com>
2021-05-04 21:00:30 +0530

    spring-security dependency in spring boot project

---
+++ spring-security/src/main/java/springbootstarter/SpringSecurityMain.java
@@ -0,0 +1,11 @@
+package springbootstarter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityMain {
+    public static void main(String[] agrs) {
+        SpringApplication.run(SpringSecurityMain.class, agrs);
+    }
+}
