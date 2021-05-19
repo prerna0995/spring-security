5fdcce97473d2376b111ebc7a203fe69177ef012
prerna <prerna.jain1@ihsmarkit.com>
2021-05-19 17:57:41 +0530

    jdbc authentication dependency

--- spring-security/src/main/java/springbootstarter/configuration/SecurityConfiguration.java
+++ spring-security/src/main/java/springbootstarter/configuration/SecurityConfiguration.java
@@ -1,13 +1,17 @@
 package springbootstarter.configuration;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+
+import javax.sql.DataSource;
 
 //@EnableWebSecurity tells application this is web security configuration
 @EnableWebSecurity
@@ -29,6 +33,18 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
                .and()
                .withUser("abc").password("abc123").roles(ADMIN);
    }
+
+   /* This part is required for jdbc authentication when we add jdbc dependency
+   @Autowired
+    DataSource dataSource;
+   @Override
+   protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+       auth.jdbcAuthentication().dataSource(dataSource).withDefaultSchema()
+               .withUser(User.withUsername("user").password("user123").roles("USER"))
+               .withUser(User.withUsername("admin").password("admin123").roles("ADMIN"));
+
+   }
+    */
 
    @Bean
     public PasswordEncoder getPasswordEncoder() {
